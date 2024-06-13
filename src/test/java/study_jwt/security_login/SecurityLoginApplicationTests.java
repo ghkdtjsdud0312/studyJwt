bd2e9f53de688c4f2e2ff817a20350853e7d3e45
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-06-13 21:04:01 +0900

    6/13 studyJwt 생성

---
+++ src/test/java/study_jwt/security_login/SecurityLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package study_jwt.security_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
