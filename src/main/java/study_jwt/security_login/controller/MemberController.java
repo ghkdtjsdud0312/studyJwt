e0670184ab43416b8c120539cb6614f8ee807ad2
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-06-14 22:35:51 +0900

    6/14 MemberController 내용추가

--- src/main/java/study_jwt/security_login/controller/MemberController.java
+++ src/main/java/study_jwt/security_login/controller/MemberController.java
@@ -2,6 +2,7 @@ package study_jwt.security_login.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -10,5 +11,12 @@ public class MemberController {
     @GetMapping("/member/save")
     public String saveForm() {
         return "save";
+    }
+
+    // 회원가입 새로운 내용 입력 저장
+    @PostMapping("/member/save")
+    public String save() {
+        System.out.println("MemberController.save");
+        return null;
     }
 }
