1747637f8389c01ab7273c4244396cbc91c26bb1
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-06-15 21:24:56 +0900

    6/15 MemberController 내용추가

--- src/main/java/study_jwt/security_login/controller/MemberController.java
+++ src/main/java/study_jwt/security_login/controller/MemberController.java
@@ -3,6 +3,7 @@ package study_jwt.security_login.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MemberController {
@@ -13,10 +14,14 @@ public class MemberController {
         return "save";
     }
 
-    // 회원가입 새로운 내용 입력 저장
+    // 회원가입 새로운 내용 입력 저장(이메일, 비밀번호, 이름)
     @PostMapping("/member/save")
-    public String save() {
+    public String save(@RequestParam("memberEmail") String memberEmail,
+                       @RequestParam("memberPassword") String memberPassword,
+                       @RequestParam("memberName") String memberName) {
         System.out.println("MemberController.save");
-        return null;
+        // soutp 하면 3가지 값을 다 볼 수 있다.
+        System.out.println("memberEmail = " + memberEmail + ", memberPassword = " + memberPassword + ", memberName = " + memberName);
+        return "index";
     }
 }
