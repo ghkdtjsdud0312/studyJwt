95dd0ef3224b66e610f199e0d12b61ccd706a0d8
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-06-15 21:46:19 +0900

    6/15 membercontroller 내용바꿈

--- src/main/java/study_jwt/security_login/controller/MemberController.java
+++ src/main/java/study_jwt/security_login/controller/MemberController.java
@@ -2,8 +2,10 @@ package study_jwt.security_login.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import study_jwt.security_login.dto.MemberDTO;
 
 @Controller
 public class MemberController {
@@ -16,12 +18,11 @@ public class MemberController {
 
     // 회원가입 새로운 내용 입력 저장(이메일, 비밀번호, 이름)
     @PostMapping("/member/save")
-    public String save(@RequestParam("memberEmail") String memberEmail,
-                       @RequestParam("memberPassword") String memberPassword,
-                       @RequestParam("memberName") String memberName) {
+    // 스프링이 제공하는 어노테이션 : @ModelAttribute는 DTO로 쓰면 내용 다 가지고 온다.
+    public String save(@ModelAttribute MemberDTO memberDTO) {
         System.out.println("MemberController.save");
         // soutp 하면 3가지 값을 다 볼 수 있다.
-        System.out.println("memberEmail = " + memberEmail + ", memberPassword = " + memberPassword + ", memberName = " + memberName);
+        System.out.println("memberDTO = " + memberDTO);
         return "index";
     }
 }
